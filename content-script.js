59d61a8c46d61127a1a566474b0ca27e93e1bb58
AlexLeoTW <alex@alexleo.click>
2024-01-05 17:59:21 +0800

    [feat] make it work on chrome as well

--- content-script.js
+++ content-script.js
@@ -1,20 +1,21 @@
-/*
-If the click was on a link, send a message to the background page.
-The message contains the link's URL.
-*/
+const browser = window.browser || window.chrome;
+
+/**
+ * If the click was on a link, send a message to the background page.
+ * The message contains the link's URL.
+ */
 function notifyExtension(e) {
   let target = e.target;
   while ((target.tagName != "A" || !target.href) && target.parentNode) {
     target = target.parentNode;
   }
-  if (target.tagName != "A")
-    return;
+  if (target.tagName != "A") return;
 
   console.log("content script sending message");
-  browser.runtime.sendMessage({"url": target.href});
+  browser.runtime.sendMessage({ url: target.href });
 }
 
-/*
-Add notifyExtension() as a listener to click events.
-*/
+/**
+ * Add notifyExtension() as a listener to click events.
+ */
 window.addEventListener("click", notifyExtension);
